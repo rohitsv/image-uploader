49cd91a15c92b3594c488dedcfbfe600c9ae5e81
rohit <rohit_v@live.com>
2023-01-15 23:50:43 +0530

    create upload form elements

--- image-uploader-ui/src/app/app.module.ts
+++ image-uploader-ui/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UploadFormComponent } from './upload-form/upload-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadFormComponent
   ],
   imports: [
     BrowserModule
